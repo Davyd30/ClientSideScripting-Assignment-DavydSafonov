e91a4074de012e9424962b1d9e4736fd6900e5c9
Davyd30 <157729152+Davyd30@users.noreply.github.com>
2024-12-05 11:48:31 +0000

    Empty project

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const teamUrl = 'https://api-nba-v1.p.rapidapi.com/teams';
+const teamOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'da7ee2d10bmsh7cd13191fe0115dp189d45jsn2f97842e1550',
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+    }
+};
+
+const playersUrl = 'https://api-nba-v1.p.rapidapi.com/players?team=1&season=2021';
+const playersOptions = {
+    method: 'GET',
+    headers: {
+        'x-rapidapi-key': 'da7ee2d10bmsh7cd13191fe0115dp189d45jsn2f97842e1550',
+        'x-rapidapi-host': 'api-nba-v1.p.rapidapi.com'
+    }
+};
